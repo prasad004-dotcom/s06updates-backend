ba061a4b05d94c229b6c50aa8256dc338085056f
Prasad <prasadlucky040904@gmail.com>
2025-03-04 18:49:16 +0530

    Initial commit with server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('S06 Updates Backend is Running');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Prasad@04',
+    database: 's06updates_db'
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+db.connect(err => {
+    if (err) {
+        console.error('Database connection failed:', err);
+        return;
+    }
+    console.log('Connected to MySQL Database');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
